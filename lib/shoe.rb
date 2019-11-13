77e2e65a899c3d3fec8cd49f4e4debd3bf0f54eb
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 04:53:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :material, :condition
+  attr_reader :brand, :size
 
 BRANDS = []
 
@@ -16,4 +16,23 @@ end
     self.condition = "new"
     puts "Your shoe is as good as new!"
     end
+<<<<<<< HEAD
 end
+=======
+
+    def size(size)
+      @size = 3
+    end
+
+
+end
+
+
+
+#def cobble
+    #puts "Your shoe is as good as new!"
+  #  @condition = "new"
+#  end
+
+#end
+>>>>>>> 334fadf7fa2a32067b16d885ff2aa7bfb3e7f92f
